19fc7d9613d81263bc0bd2f86b2fc2705d03374c
Bentelador <ravenpalacio7@gmail.com>
2025-12-04 16:53:40 +0800

    Update js.js

--- js.js
+++ js.js
@@ -3,32 +3,30 @@ async function ben(serch, BS){
   await fetch('https://raw.githubusercontent.com/Bentelador/movie-bai/refs/heads/main/MDB.json')
     .then(response => response.json())
     .then(jsonData => {
-      const cont = BS.children
-      const arr = []
-      for (let i = 0; i < cont.length; i++) {
-        if (cont[i].checked == true) {
-        arr.push(cont[i].name)
-        }
-      }
-      const res = jsonData.filter(n => arr.every(genreArray => n.genre.includes(genreArray)))
+      const res = jsonData.filter(n => BS.every(genreArray => n.genre.includes(genreArray)))
       result = res.filter(n => n.title.toLowerCase().includes(serch));
     })
   return result;
 }
 
-async function genre(BS){
+async function genre(BS, str){
   const cont = BS.children
   const arr = []
+  const newstr = str
     for (let i = 0; i < cont.length; i++) {
       if (cont[i].checked == true) {
         arr.push(cont[i].name)
       }
     }
-  return arr
+  arr.foreach(Gen => {
+    newstr += "&genre="Gen
+  }
+  return newstr
 }
 
 export default ben
 export { genre }
+
 
 
 
